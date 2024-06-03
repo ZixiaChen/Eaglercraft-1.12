487162c830e2322e0c7db7f81e3c505b6d167ef0
CatFoolYou <catfoolyou.github@manyme.com>
2024-06-03 13:33:43 +0000

    More LWJGL stuff

--- src/main/java/net/minecraft/src/DisplayModeComparator.java
+++ src/main/java/net/minecraft/src/DisplayModeComparator.java
@@ -1,14 +1,14 @@
 package net.minecraft.src;
 
 import java.util.Comparator;
-import org.lwjgl.opengl.DisplayMode;
+import net.lax1dude.eaglercraft.v1_8.Display;
 
 public class DisplayModeComparator implements Comparator
 {
     public int compare(Object p_compare_1_, Object p_compare_2_)
     {
-        DisplayMode displaymode = (DisplayMode)p_compare_1_;
-        DisplayMode displaymode1 = (DisplayMode)p_compare_2_;
+        Display displaymode = (Display)p_compare_1_;
+        Display displaymode1 = (Display)p_compare_2_;
 
         if (displaymode.getWidth() != displaymode1.getWidth())
         {
@@ -18,13 +18,7 @@ public class DisplayModeComparator implements Comparator
         {
             return displaymode.getHeight() - displaymode1.getHeight();
         }
-        else if (displaymode.getBitsPerPixel() != displaymode1.getBitsPerPixel())
-        {
-            return displaymode.getBitsPerPixel() - displaymode1.getBitsPerPixel();
-        }
-        else
-        {
-            return displaymode.getFrequency() != displaymode1.getFrequency() ? displaymode.getFrequency() - displaymode1.getFrequency() : 0;
-        }
+
+        return 0;
     }
 }
